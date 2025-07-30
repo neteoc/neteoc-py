062d1835c0312e5ff0f7ce82fc6d241c95b44fb5
Kerry <kerry.hatcher@coxautoinc.com>
2025-07-30 15:11:07 -0400

    Remove map-related files and code
    - Deleted SVG file for map pin icon.
    - Removed CSS file for map styling.
    - Eliminated JavaScript file responsible for map functionality.
    - Deleted HTML template for rendering the map.
    - Removed test file for markers.
    - Cleared URL configuration for map view.
    - Erased view class handling map data and context.

--- markers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis import admin
-from markers.models import Marker
-
-@admin.register(Marker)
-class MarkerAdmin(admin.GISModelAdmin):
-    list_display = ("name", "address")
-    search_fields = ("name", "address")
-
-
--- markers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from markers.views import MapView
-
-urlpatterns = [
-    path("map/", MapView.as_view()),
-    
-]
